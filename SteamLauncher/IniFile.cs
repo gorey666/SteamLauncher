db17ee2a0f7e06ef66530e9466b2e3091392cceb
Corey Harwell <coreyharwell@gmail.com>
2018-04-17 00:10:38 -0500

    Add project files.

---
+++ SteamLauncher/IniFile.cs
@@ -0,0 +1,32 @@
+﻿// SteamLauncher: Copyright ©  2018 - Corey Harwell
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SteamLauncher
+{
+    class IniFile
+    {
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString( string section, string key, string val, string filePath );
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString( string section, string key, string def, StringBuilder value, int size, string filePath );
+
+        public string FileName { get; private set; }
+        private StringBuilder _readValue;
+
+        public IniFile( string filePath )
+        {
+            FileName = filePath;
+            _readValue = new StringBuilder(255);
+        }
+        public void WriteValue( string Section, string Key, string Value )
+        {
+            WritePrivateProfileString(Section, Key, Value, FileName);
+        }
+        public string ReadValue( string Section, string Key )
+        {
+            GetPrivateProfileString(Section, Key, "", _readValue, _readValue.Capacity, FileName);
+            return _readValue.ToString();
+        }
+    }
+}
---
+++ SteamLauncher/Program.cs
@@ -0,0 +1,45 @@
+﻿// SteamLauncher: Copyright ©  2018 - Corey Harwell
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace SteamLauncher
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string appID = string.Empty;
+            IniFile ini = new IniFile(".\\SteamLauncher.ini");
+
+            if (args.Length > 0)
+                appID = args[0];
+            else if (File.Exists(ini.FileName))
+                appID = ini.ReadValue("Launch", "AppID");
+            else
+                ini.WriteValue("Launch", "AppID", ""); //<--- creates the file if it is missing
+
+            if (appID == string.Empty)
+            {
+                MessageBox.Show(
+                    "You must specify the App to launch by adding it's ID to the SteamLauncher.ini file.",
+                    "Launch Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                ProcessStartInfo app = new ProcessStartInfo("steam://rungameid/" + appID);
+                app.UseShellExecute = true;
+                Process.Start(app);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Launch Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
